7467366dfaccd2327703f34c97ba17f4770bf268
Jerry Chang <fx2ptt@gmail.com>
2025-10-13 21:44:31 +0800

    v-model checkbox

--- src/components/VmodelDemo.vue
+++ src/components/VmodelDemo.vue
@@ -3,6 +3,8 @@ import { ref } from 'vue'
 
 const text = ref('')
 const message = ref('')
+
+const interests = ref([])
 </script>
 
 <template>
@@ -18,6 +20,14 @@ const message = ref('')
     <p style="white-space: pre-line">{{ message }}</p>
     <textarea v-model="message" placeholder="add multiple lines" rows="4" cols="50"></textarea>
   </div>
+
+  <hr />
+  <h3>選擇你的興趣：</h3>
+  <label><input type="checkbox" value="閱讀" v-model="interests" /> 閱讀</label>
+  <label><input type="checkbox" value="運動" v-model="interests" /> 運動</label>
+  <label><input type="checkbox" value="旅行" v-model="interests" /> 旅行</label>
+
+  <p>你選擇的興趣：{{ interests.join(', ') }}</p>
 </template>
 
 <style scoped></style>
