6d542aab2bdd32f3d5c3bc28468f24c1ca11aaac
Jerry Chang <fx2ptt@gmail.com>
2025-10-27 19:36:26 +0800

    修改 router import 寫法

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 // src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '@/views/Home.vue'
-import About from '@/views/About.vue'
 import UserPost from '@/views/UserPost.vue'
 import Users from '@/views/Users.vue'
 import UsersList from '@/views/UsersList.vue'
@@ -9,8 +7,8 @@ import UserspPofile from '@/views/UserspPofile.vue'
 import UsersData from '@/views/UsersData.vue'
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/about', component: About },
+  { path: '/', component: () => import('@/views/Home.vue') },
+  { path: '/about', component: () => import('@/views/About.vue') },
   { path: '/users/:username/posts/:postId', component: UserPost },
   {
     path: '/users',
