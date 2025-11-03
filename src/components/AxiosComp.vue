6d3eae209c3b01bf43fcb927cf375babe0276dd0
Jerry Chang <fx2ptt@gmail.com>
2025-11-03 20:43:06 +0800

    套用 element Table

--- src/components/AxiosComp.vue
+++ src/components/AxiosComp.vue
@@ -1,5 +1,8 @@
 <script setup>
+import { reactive } from 'vue'
 import axios from 'axios'
+
+const tableData = reactive([])
 
 let apiService = axios.create({
   baseURL: 'http://localhost:8080',
@@ -9,13 +12,24 @@ const queryProduct = () => {
   // GET
   apiService
     .get('/api/queryAllProducts')
-    .then((response) => console.log(response.data))
+    .then((response) => {
+      console.log('🚀 ~ queryProduct ~ response:', response)
+      Object.assign(tableData, response.data)
+    })
     .catch((error) => console.error(error))
 }
 </script>
 
 <template>
   <div>
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column prop="id" label="ID" width="180" />
+      <el-table-column prop="name" label="Name" width="180" />
+      <el-table-column prop="product_price" label="Price" />
+      <el-table-column prop="description" label="Description" />
+      <el-table-column prop="image" label="Image" />
+    </el-table>
+
     <el-button type="primary" @click="queryProduct()">查詢產品</el-button>
   </div>
 </template>
